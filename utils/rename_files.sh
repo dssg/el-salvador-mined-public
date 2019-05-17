971147699320069b01ff432663916fcf154eb73c
astern <alena7690@gmail.com>
2019-05-17 11:40:32 -0500

    remove dssg files from master

--- utils/rename_files.sh
+++
@@ -1,5 +0,0 @@
-for f in *.csv
-do
-    fn="${f%.*}"
-    mv $f "$fn""-B.csv"
-done
--- utils/start_jupyter.sh
+++
@@ -1,7 +0,0 @@
-if [ $# -eq 0 ]
-  then
-    echo "Missing port number argument. Please choose a number beteween 0-9."
-  else
-    source .venv/bin/activate
-    jupyter notebook --no-browser --port=800"$1"
-fi
